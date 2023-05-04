291b2cf987958ca28f466803a3ac680b3d853ca0
reinhard <reinhard2057@gmail.com>
2023-05-04 20:25:37 +0000

    task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- script that creates a tabe users with these attr
+-- id, email, name...
+CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255)
+        );
