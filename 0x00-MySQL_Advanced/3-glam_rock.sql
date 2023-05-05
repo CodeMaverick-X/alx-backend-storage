ae4ebabdb45811502c52babf4e634c0037f4625c
reinhard <reinhard2057@gmail.com>
2023-05-05 18:59:08 +0000

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- their main style, ranked by thier longevity
 SELECT `band_name`, IFNULL(split, 2020)-formed AS `lifespan`
   FROM `metal_bands`
- WHERE style='Glam rock'
+ WHERE style LIKE '%Glam rock%'
  ORDER BY lifespan DESC;
