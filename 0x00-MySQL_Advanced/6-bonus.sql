90b77e3d12e6d4328f9077044efe01ec23b70502
reinhard <reinhard2057@gmail.com>
2023-05-05 21:58:10 +0000

    task 6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,19 @@
+-- sql script that contains a stored procedure `AddBonus`ù
+-- that adds a new corr for a student
+DELIMITER $$
+CREATE
+DEFINER = CURRENT_USER
+PROCEDURE AddBonus ( IN user_id INTEGER, IN project_name varchar(255), IN score INTEGER)
+COMMENT 'aded corr for student'
+LANGUAGE SQL
+DETERMINISTIC
+BEGIN
+	DECLARE project_id INTEGER;
+	SET project_id = (SELECT id FROM projects WHERE `name`=project_name);
+	IF project_id IS NULL THEN
+		INSERT INTO projects (name) VALUES (project_name);
+		SET project_id = LAST_INSERT_ID();
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, project_id, score);
+END$$
