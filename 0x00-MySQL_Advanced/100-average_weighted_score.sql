692e8a08265e52ef18a6d972e0456520182704f1
reinhard <reinhard2057@gmail.com>
2023-05-06 18:56:39 +0000

    task 100

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,11 @@
+-- a SQL script that creates a stored procedure 
+-- ComputeAverageWeightedScoreForUser that computes
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    SELECT `corrections`.`score`, `projects`.`weight`/100 as `weight`
+    FROM `corrections` INNER JOIN `projects`
+    ON `corrections`.`project_id` = `projects`.`id`
+    
+END$$
+DELIMITER ;
