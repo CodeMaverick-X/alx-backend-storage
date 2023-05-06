978cb20b3ad7946eddba6dac6765d8c6037f4125
reinhard <reinhard2057@gmail.com>
2023-05-06 18:57:52 +0000

    task 100

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,9 +3,8 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-    SELECT `corrections`.`score`, `projects`.`weight`/100 as `weight`
+    SELECT `corrections`.`score`, `projects`.`weight` / 100 as `weight`
     FROM `corrections` INNER JOIN `projects`
     ON `corrections`.`project_id` = `projects`.`id`
     
-END$$
-DELIMITER ;
+END $$
