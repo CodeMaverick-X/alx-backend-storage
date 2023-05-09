01c069be4ac808ad0d73571c23ab73671ff4ec07
reinhard <reinhard2057@gmail.com>
2023-05-09 19:51:22 +0000

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient as mc
 
 
 def main():
-    """main func"""
+    """main func: entry point"""
     with mc() as client:
         db = client.logs
         coll = db.nginx
@@ -17,17 +17,17 @@ def main():
         print(no_of_docs, "logs")
         print('Methods:')
         no_of_get = coll.count_documents({"method": "GET"})
-        print(f'    method GET: {no_of_get}')
+        print('    method GET: {}'.format(no_of_get))
         no_of_post = coll.count_documents({"method": "POST"})
-        print(f'    method POST: {no_of_post}')
+        print('    method POST: {}'.format(no_of_post))
         no_of_put = coll.count_documents({"method": "PUT"})
-        print(f'    method PUT: {no_of_put}')
+        print('    method PUT: {}'.format(no_of_put))
         no_of_patch = coll.count_documents({"method": "PATCH"})
-        print(f'    method PATCH: {no_of_patch}')
+        print('    method PATCH: {}'.format(no_of_patch))
         no_of_del = coll.count_documents({"method": "DELETE"})
-        print(f'    method DELETE: {no_of_del}')
+        print('    method DELETE: {}'.format(no_of_del))
         stat_ck = coll.count_documents({"method": "GET", "path": "/status"})
-        print(f'{stat_ck} status check')
+        print('{} status check'.format(stat_ck))
 
 
 if __name__ == "__main__":
