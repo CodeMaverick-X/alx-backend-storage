c9996d259b351a236bf18fb5111ecb7c5e589340
reinhard <reinhard2057@gmail.com>
2023-05-10 22:21:24 +0000

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,11 +11,11 @@ class Cache:
     """class that creates a cache mech"""
     def __init__(self):
         """instatiate"""
-        self.__redis = redis.Redis()
-        self.__redis.flushdb()
+        self.___redis = redis.Redis()
+        self.___redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store value in cache"""
         key = str(uuid.uuid4())
-        self.__redis.set(key, data)
+        self.___redis.set(key, data)
         return key
