a97d14efbff2866d7006004617361fc06a67f939
reinhard <reinhard2057@gmail.com>
2023-05-10 22:18:42 +0000

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+contains a cache class
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        """instatiate"""
+        self.__redis = redis.Redis()
+        self.__redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """store value in cache"""
+        key = str(uuid.uuid4())
+        self.__redis.set(key, data)
+        return key
