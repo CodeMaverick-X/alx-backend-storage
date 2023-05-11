86702dbf67fae49b547321055a984f0ff637f470
reinhard <reinhard2057@gmail.com>
2023-05-11 14:24:11 +0000

    task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,24 @@ contains a cache class
 """
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Any
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """decorate stored method to count no of calls"""
+    @wraps(method)
+    def wrapped(*args, **kwargs) -> Any:
+        """A wrapper function"""
+        if "Cache.{}".format(method.__name__) == method.__qualname__:
+            self = args[0]
+            key = method.__qualname__
+            if not self._redis.get(key):
+                self._redis.set(key, 1)
+            else:
+                self._redis.incr(key)
+        return method(*args, **kwargs)
+    return wrapped
 
 
 class Cache:
@@ -15,6 +32,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store value in cache"""
         key = str(uuid.uuid4())
