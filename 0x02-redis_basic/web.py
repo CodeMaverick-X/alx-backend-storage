64f841821a8c10b2ffaacf39cf8a75118a948f2e
reinhard <reinhard2057@gmail.com>
2023-05-11 16:08:58 +0000

    task adv

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+contains function get_page
+get html content and return it
+
+"""
+import requests
+import redis
+from functools import wraps
+from datetime import timedelta
+from typing import Callable
+redis = redis.Redis()
+
+
+def get_page_decorator(fn: Callable[[str], str]) -> Callable:
+    """Decorates get_page"""
+    @wraps(fn)
+    def wrapper(*args, **kwargs) -> str:
+        """Wrapped get_page"""
+        url = args[0]
+        redis.incr(f"count:{url}")
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@get_page_decorator
+def get_page(url: str) -> str:
+    """Get page"""
+    html = requests.get(url, headers={"User-Agent": "Requests"}).content
+    return html
