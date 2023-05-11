b2233071367de0397fe1368fd7dd3c8933b6bda5
reinhard <reinhard2057@gmail.com>
2023-05-11 16:32:29 +0000

    task adv

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,22 +9,15 @@ import redis
 from functools import wraps
 from datetime import timedelta
 from typing import Callable
-redis = redis.Redis()
 
 
-def get_page_decorator(fn: Callable[[str], str]) -> Callable:
-    """Decorates get_page"""
-    @wraps(fn)
-    def wrapper(*args, **kwargs) -> str:
-        """Wrapped get_page"""
-        url = args[0]
-        redis.incr(f"count:{url}")
-        return fn(*args, **kwargs)
-    return wrapper
-
-
-@get_page_decorator
 def get_page(url: str) -> str:
     """Get page"""
-    html = requests.get(url, headers={"User-Agent": "Requests"}).content
-    return html
+    r = redis.Redis()
+    count = r.incr("count:"+url)
+    page = r.get('cache_page')
+    if not page:
+        page = requests.get(url, headers={"User-Agent": "Requests"}).content
+        res = r.setex('cache_page', 10, page)
+
+    return page
