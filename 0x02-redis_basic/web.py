9e4e5beb0fed6d698615f3e6da625c6d05c14ff4
reinhard <reinhard2057@gmail.com>
2023-05-11 16:37:13 +0000

    task adv

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,7 +14,7 @@ from typing import Callable
 def get_page(url: str) -> str:
     """Get page"""
     r = redis.Redis()
-    count = r.incr("count:"+url)
+    count = r.incr("count: "+url)
     page = r.get('cache_page')
     if not page:
         page = requests.get(url, headers={"User-Agent": "Requests"}).content
